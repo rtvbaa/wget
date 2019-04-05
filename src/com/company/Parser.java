fb0443d187b2dcbca3d4abc0cc995691e5564041
AndreyB <myEmail@example.com>
2019-04-05 13:22:45 +0300

    refactoring

--- src/com/company/Parser.java
+++ src/com/company/Parser.java
@@ -7,13 +7,15 @@ public class Parser {
     public static String foldername;
     public static String url;
 
-    public static void parser(String[] args) {
+    public static String parser(String[] args) {
+        args.toString().trim().split(" +");
         if (args[0].contains("-n")) numofthreads = args[0];
         else if (args[0].contains("-l")) speed = args[0];
         else if (args[0].contains("-f")) filepath = args[0];
         else if (args[0].contains("-o")) foldername = args[0];
         else url = args[0];
 
-        System.out.println(numofthreads);
+        System.out.println(url);
+        return url;
     }
 }
