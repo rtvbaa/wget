765a33a237145fb6791943872496027040af0919
A <a@a.a>
2019-04-30 00:47:21 +0300

    refactoring

--- src/com/company/Download.java
+++ src/com/company/Download.java
@@ -33,57 +33,23 @@ public class Download implements Runnable {
             }
 
             statistics.start();
+            speedLimit.start();
 
             InputStream inputStream = url.openStream();
             FileOutputStream fileOutputStream = new FileOutputStream(file);
             byte dataBuffer[] = new byte[Config.buffersize];
             int bytesRead;
             while ((bytesRead = inputStream.read(dataBuffer, 0, Config.buffersize)) != -1) {
-                speedLimit.start();
-                filesize = filesize + dataBuffer.length;
+                filesize = filesize + bytesRead;
                 fileOutputStream.write(dataBuffer, 0, bytesRead);
-                speedLimit.finish();
+                speedLimit.finish(filesize);
             }
             System.out.println("Размер файла " + file.length());
             statistics.addBytes(filesize);
             statistics.finish();
             statistics.showResult();
-        } catch (IOException | InterruptedException e) {
+        } catch (Exception e) {
+            System.out.println("ОШИБКА ЗАГРУЗКИ");
         }
-//        try {
-//            InputStream inputStream = url.openStream();
-//            BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
-//
-//            Path path = new File(Config.getFoldername() + "/" + filename).toPath();
-//            if (Files.exists(path)) {
-//                Files.delete(path);
-//            }
-
-
-
-
-
-//            File file = new File(Config.getFoldername() + "/" + filename);
-//            FileOutputStream fileOutputStream = new FileOutputStream(file);
-//
-//            byte[] buffer = new byte[1024];
-//
-//            long filesize = 0;
-//
-//
-//            while (inputStream.available() > 0)
-//            {
-////                SpeedLimit.start();
-//                int count = inputStream.read(buffer);
-////                filesize = filesize + count;
-//                fileOutputStream.write(buffer, 0, count);
-////                SpeedLimit.finish();
-//            }
-//            Statistics.addBytes(filesize);
-//            fileOutputStream.close();
-
-//        } catch(IOException  e){
-//            e.printStackTrace();
-//        }
     }
 }
--- src/com/company/SpeedLimit.java
+++ src/com/company/SpeedLimit.java
@@ -6,16 +6,16 @@ public class SpeedLimit {
     private Date finish;
     private Date start;
     private long time;
+    private long millis;
 
     public void start() {
         start = new Date();
     }
 
-    public void finish() throws InterruptedException {
+    public void finish(long filesize) throws InterruptedException {
         finish = new Date();
         time = finish.getTime() - start.getTime();
-        if ((Config.buffersize / Config.getSpeed() - this.time) > 0){
-            Thread.sleep(Config.buffersize / Config.getSpeed() - this.time);
-        }
+        millis = filesize / Config.getSpeed() - time;
+        if (millis > 0) Thread.sleep(millis);
     }
 }
